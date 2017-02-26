3bded263d721a4b21149dddc5066f449aea530a7
brycelambert <brycelambert@gmail.com>
2017-02-26 15:34:51 -0500

    move code into Neeed class; work on item fetch

--- neeed_backup.rb
+++ neeed_backup.rb
@@ -2,30 +2,16 @@ require 'open-uri'
 require 'mechanize'
 require 'pry'
 
+class Item
+  attr_accessor :id, :name, :vendor, :url, :price, :image_url
+end
+
 class List
   attr_accessor :name, :id, :items
 
   def initialize(name, id)
     self.name = name
     self.id = id
-  end
-
-  def self.fetch_lists(neeed)
-    neeed.homepage.css('li[data-list]').collect do |list_el|
-      url = list_el.elements.attribute('href').value
-      name, id = url.split('l-').last.split('~')
-      List.new(name, id)
-    end
-  end
-
-  def fetch_items(neeed, offset=20)
-    result = neeed.get(url(offset))
-
-    unless result.links.blank?
-      result.css('div[data-product]').map { |item| add_item(item) }
-      fetch_items(neeed, offset+20)
-    end
-
   end
 
   def output
@@ -37,64 +23,84 @@ class List
     #create one big csv
   end
 
-  def add_item(item_source)
-    item = Item.new
-    item.id = item_source.attributes['id'].value
-    item.image_url = item_source.css('.prod-img').first.css('img').first.attributes['src'].value
-    item.name = item_source.css('.prod-img').first.css('img').first.attributes['alt'].value
-    item.vendor = item_source.css("a[itemprop='brand']").text.strip
-    item.url = get( item_source.css('.prod-img').first.attributes['href'].value ).uri.to_s
-    item.price = item_source.css('.price').text
-
-    self.items.push(item)
-  end
-
   private
-
-  def url(offset)
-    "http://neeed.com/core/process/aru.get.php?t=user&l=#{self.name}&s=#{offset}&ui=#{id}&p=&cols=3"
-  end
 
   def make_csv_line
   end
 
 end
 
-class Item
-  attr_accessor :id, :name, :vendor, :url, :price, :image_url
-end
-
-
-class NeeedSession
-  attr_accessor :agent
+class NeeedAgent
+  attr_accessor :agent, :u_id
 
   def initialize(user, pass)
-    login(user, pass)
     @agent = Mechanize.new
+    login(user, pass)
+  end
+
+  def fetch_lists
+    homepage.css('li[data-list]').collect do |list_el|
+      url = list_el.elements.attribute('href').value
+      name, id = url.split('l-').last.split('~')
+      List.new(name, id)
+    end
+  end
+
+  def fetch_items(list, offset=20)
+    result = agent.get(list_url(list.name, offset))
+
+    if result.links.empty?
+      []
+    else
+      result.css('div[data-product]').map { |item|
+        build_item(item)
+      }.concat( fetch_items(list, offset+20) )
+    end
+  end
+
+
+  private
+
+  def build_item(item_source)
+    item = Item.new
+    item.id = item_source.attributes['id'].value
+    item.image_url = item_source.css('.prod-img').first.css('img').first.attributes['src'].value
+    item.name = item_source.css('.prod-img').first.css('img').first.attributes['alt'].value
+    item.vendor = item_source.css("a[itemprop='brand']").text.strip
+    item.price = item_source.css('.price').text
+
+    item.url =
+      begin
+        agent.get( item_source.css('.prod-img').first.attributes['href'].value ).uri.to_s
+      rescue Mechanize::ResponseCodeError
+        agent.history[-1].uri.to_s
+      end
   end
 
   def homepage
-    agent.get 'http://neeed.com'
+    self.agent.get 'http://neeed.com'
   end
 
-  private
+  def list_url(list_name, offset)
+    "http://neeed.com/core/process/aru.get.php?t=user&l=#{list_name}&s=#{offset}&ui=#{u_id}&p=&cols=3"
+  end
 
   def login(user, pass)
     form = agent.get("http://neeed.com/core/static/login.box.static.php").form
     form.email = user
     form.password = pass
     form.submit
+    @u_id = homepage.xpath("//*[@data-usr]").first.attributes['data-usr'].value.to_i
   end
-
 end
 
 
 def backup(username, pass)
-  neeed = NeeedSession.new(username, pass)
-  lists = List.fetch_lists(neeed.agent)
-  lists.map { |list| list.fetch_items(neeed.agent) }
+  neeed = NeeedAgent.new(username, pass)
+  lists = neeed.fetch_lists
+  lists.each { |list| lists.items = neeed.fetch_items(list) }
 
-  # lists.map { |list| list.output }
+  #lists.each { |list| list.output }
   # List.output_all(lists)
 end
 
